bc2e7388d48eb4397237f2162bb9ee4e17725e96
Hardik Parab <hrparab99@gmail.com>
2025-03-08 13:46:05 +0530

    Update solution_day_2.0_explorer.sh

--- Challenges/Day_2/solution_day_2.0_explorer.sh
+++ Challenges/Day_2/solution_day_2.0_explorer.sh
@@ -3,8 +3,7 @@
 # ls -lh → Lists files and directories with human-readable sizes.
 # tail -n +2 → Skips the first line (total ...).
 # awk '{print "- " $9 " (" $5 ")"}' → Formats output as - filename (size).
-# sort -k9 → sort the column 9 of output in alphabatical order
 # | → used to pass the output of one command as input to another command.
 
 echo -e "\nFiles and Directories in the Current Path:"
-ls -lh | tail -n +2 | awk '{print "- " $9 " ("$5") "}' | sort -k9
+ls -lh | tail -n +2 | awk '{print "- " $9 " ("$5") "}'
