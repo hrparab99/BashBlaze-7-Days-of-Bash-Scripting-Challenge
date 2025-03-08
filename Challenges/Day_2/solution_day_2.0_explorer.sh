c45f2cf6a16b54be47bd541a3cd64f0fde1628a3
Hardik Parab <hrparab99@gmail.com>
2025-03-08 13:18:28 +0530

    Update and rename explorer.sh to solution_day_2.0_explorer.sh

---
+++ Challenges/Day_2/solution_day_2.0_explorer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# ls -lh → Lists files and directories with human-readable sizes.
+# tail -n +2 → Skips the first line (total ...).
+# awk '{print "- " $9 " (" $5 ")"}' → Formats output as - filename (size).
+# sort -k9 → sort the column 9 of output in alphabatical order
+# | → used to pass the output of one command as input to another command.
+
+echo "Files and Directories in the Current Path:"
+ls -lh | tail -n +2 | awk '{print "- " $9 " ("$5") "}' | sort -k9
