b0463ebe32f5c64dee11f51e2a5dcb453c5e03ea
Hardik Parab <hrparab99@gmail.com>
2025-03-08 13:20:20 +0530

    Update solution_day_2.0_explorer.sh

--- Challenges/Day_2/solution_day_2.0_explorer.sh
+++ Challenges/Day_2/solution_day_2.0_explorer.sh
@@ -6,5 +6,5 @@
 # sort -k9 → sort the column 9 of output in alphabatical order
 # | → used to pass the output of one command as input to another command.
 
-echo "Files and Directories in the Current Path:"
+echo -e "\nFiles and Directories in the Current Path:"
 ls -lh | tail -n +2 | awk '{print "- " $9 " ("$5") "}' | sort -k9
