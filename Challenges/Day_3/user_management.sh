d63411f5d06074bec56c06d5ead2ad3e93ef81d1
Hardik Parab <hrparab99@gmail.com>
2025-03-20 19:02:32 +0530

    Update user_management.sh

--- Challenges/Day_3/user_management.sh
+++ Challenges/Day_3/user_management.sh
@@ -13,55 +13,22 @@ function display_usage {
 
 # Function to create a new user account
 function create_user {
-    read -p "Enter the new username: " username
-
-    # Check if the username already exists
-    if id "$username" &>/dev/null; then
-        echo "Error: The username '$username' already exists. Please choose a different username."
-    else
-        # Prompt for password (Note: You might want to use 'read -s' to hide the password input)
-        read -p "Enter the password for $username: " password
-
-        # Create the user account
-        useradd -m -p "$password" "$username"
-        echo "User account '$username' created successfully."
-    fi
+        ./create_user.sh
 }
 
 # Function to delete an existing user account
 function delete_user {
-    read -p "Enter the username to delete: " username
-
-    # Check if the username exists
-    if id "$username" &>/dev/null; then
-        userdel -r "$username"  # -r flag removes the user's home directory
-        echo "User account '$username' deleted successfully."
-    else
-        echo "Error: The username '$username' does not exist. Please enter a valid username."
-    fi
+        ./delete_user.sh
 }
 
 # Function to reset the password for an existing user account
 function reset_password {
-    read -p "Enter the username to reset password: " username
-
-    # Check if the username exists
-    if id "$username" &>/dev/null; then
-        # Prompt for password (Note: You might want to use 'read -s' to hide the password input)
-        read -p "Enter the new password for $username: " password
-
-        # Set the new password
-        echo "$username:$password" | chpasswd
-        echo "Password for user '$username' reset successfully."
-    else
-        echo "Error: The username '$username' does not exist. Please enter a valid username."
-    fi
+        ./reset_password.sh
 }
 
 # Function to list all user accounts on the system
 function list_users {
-    echo "User accounts on the system:"
-    cat /etc/passwd | awk -F: '{ print "- " $1 " (UID: " $3 ")" }'
+        ./list_users.sh
 }
 
 # Check if no arguments are provided or if the -h or --help option is given
@@ -72,23 +39,23 @@ fi
 
 # Command-line argument parsing
 while [ $# -gt 0 ]; do
-    case "$1" in
-        -c|--create)
-            create_user
-            ;;
-        -d|--delete)
-            delete_user
-            ;;
-        -r|--reset)
-            reset_password
-            ;;
-        -l|--list)
-            list_users
-            ;;
-        *)
-            echo "Error: Invalid option '$1'. Use '--help' to see available options."
-            exit 1
-            ;;
-    esac
-    shift
+        case "$1" in
+                -c | --create)
+                        create_user
+                        ;;
+                -d | --delete)
+                        delete_user
+                        ;;
+                -r | --reset)
+                        reset_password
+                        ;;
+                -l | --list)
+                        list_users
+                        ;;
+                *)
+                        echo "Error : Invalid option '$1'. Use '--help' to see available options."
+                        exit 1
+                        ;;
+        esac
+        shift
 done
